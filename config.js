0981439a0c6699edf0c32a02529742505dc73cc9
Maurice Gift <maurice@giftedtech.co.ke>
2026-04-05 23:14:29 +0300

    🔧 Restore original MSG_FOOTER unicode characters

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL,
     BOT_REPO: process.env.BOT_REPO || "https://github.com/mauricegift/atassa",
     WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VbCpYtZLtOj5LDuj7Q1p",
-    MSG_FOOTER: process.env.MSG_FOOTER || "> *\u1d18\u1d0f\u1d21\u1d07\u0280\u1d07\u1d05 \u0299\u028f \u0262\u026a\u0493\u1d1b\u1d07\u1d05 \u1d1b\u1d07\u1d04\u029c*",
+    MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢɪғᴛᴇᴅ ᴛᴇᴄʜ*",
 };
